fdf41098f4628beed717d0241f7b73ec26285b6a
Хилола <rasulovahilola3@gmail.com>
2026-09-16 15:26:22 +0300

    Task1: статистика случайного массива

--- Task1.Statistics/Program.cs
+++ Task1.Statistics/Program.cs
@@ -1,11 +1,34 @@
-using System;
-namespace Task1
+namespace Task1;
+
+class Program
 {
-   class Program
-   {
-     static void Main()
-     {
-       Console.WriteLine("Hello, World!");
-     }
-   }
+  static void Main()
+  {
+    Random rnd = new Random();
+    int[] arr = new int[10];
+    for (int i = 0; i < arr.Length; i++)
+      arr[i] = rnd.Next(1, 101);
+
+    Console.WriteLine("Массив: " + string.Join(", ", arr));
+
+    int sum = 0;
+    long product = 1;
+    int evenCount = 0;
+    foreach (int x in arr)
+    {
+      sum += x;
+      product *= x;
+      if (x % 2 == 0) evenCount++;
+    }
+
+    double avg = (double)sum / arr.Length;
+    int aboveAvg = 0;
+    foreach (int x in arr)
+      if (x > avg) aboveAvg++;
+
+    Console.WriteLine($"Сумма: {sum}");
+    Console.WriteLine($"Произведение: {product}");
+    Console.WriteLine($"Чётных чисел: {evenCount}");
+    Console.WriteLine($"Больше среднего ({avg:F1}): {aboveAvg}");
+  }
 }
